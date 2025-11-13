f98964b43cf3f6b3645f530ef0c21d6c46fa68a6
Jeremy de Leon <jeremy.deleon@norimaconsulting.com>
2025-11-13 15:39:27 -0600

    comment out defaultformatter for typescript

--- src/services/fighter/fighter.service.ts
+++ src/services/fighter/fighter.service.ts
@@ -8,49 +8,51 @@ export class FighterService {
   async getFighters(): Promise<CreateFighterDto[]> {
     const filePath = path.resolve(__dirname, '../../../src/db/fighters.json');
     const data = await fs.readFile(filePath, 'utf-8');
-    
+
     return JSON.parse(data);
   }
 
   async getFighter(id): Promise<CreateFighterDto> {
     const filePath = path.resolve(__dirname, '../../../src/db/fighters.json');
     const data = await fs.readFile(filePath, 'utf-8');
-    
+
     const fighters = JSON.parse(data) as CreateFighterDto[];
 
-    if(fighters && fighters.length > 0) {
-      const fighter = fighters.find(f => f.id === parseInt(id));
+    if (fighters && fighters.length > 0) {
+      const fighter = fighters.find((f) => f.id === parseInt(id));
 
-      if(!fighter) {
-        throw new HttpException('Fighter not found', 404);  
+      if (!fighter) {
+        throw new HttpException('Fighter not found', 404);
       }
 
       return fighter;
     }
-    throw new HttpException('No fighters available', 404);  
+    throw new HttpException('No fighters available', 404);
   }
 
-  async addFighter(createFighterDto: CreateFighterDto): Promise<CreateFighterDto> 
-  {
+  async addFighter(
+    createFighterDto: CreateFighterDto,
+  ): Promise<CreateFighterDto> {
     const filePath = path.resolve(__dirname, '../../../src/db/fighters.json');
     const fileData = fs.readFile(filePath, 'utf-8');
-    const fighters = JSON.parse(await fileData) as CreateFighterDto[];  
+    const fighters = JSON.parse(await fileData) as CreateFighterDto[];
 
-    const lastId = fighters.sort(f => f.id).findLast(f => f.id)?.id || 0;
-    
-    fighters.push(new CreateFighterDto({
-      id: lastId + 1,
-      firstName: createFighterDto.firstName,
-      lastName: createFighterDto.lastName,
-      age: createFighterDto.age
-    }) 
-  );
-    
+    const lastId = fighters.sort((f) => f.id).findLast((f) => f.id)?.id || 0;
+
+    fighters.push(
+      new CreateFighterDto({
+        id: lastId + 1,
+        firstName: createFighterDto.firstName,
+        lastName: createFighterDto.lastName,
+        age: createFighterDto.age,
+      }),
+    );
+
     const finalData = JSON.stringify(fighters, null, 2);
     console.log('Updated Fighters:', finalData);
-    
+
     try {
-      fs.writeFile(filePath,  finalData);
+      fs.writeFile(filePath, finalData);
     } catch (error) {
       console.error('Error writing to file:', error);
     }
