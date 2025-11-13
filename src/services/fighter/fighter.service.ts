b8d6ddaa2d6b54303ac6494f797bec5a8942d0e2
Jeremy de Leon <jeremy.deleon@norimaconsulting.com>
2025-11-13 10:02:59 -0600

    implement read/write functionality

--- src/services/fighter/fighter.service.ts
+++ src/services/fighter/fighter.service.ts
@@ -1,16 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { CreateFighterDto } from 'src/mma/dtos/create-fighter.dto';
+import fs from 'fs';
+import { StreamArray } from 'stream-json/streamers/StreamArray.js';
 
 @Injectable()
 export class FighterService {
   getFighters(): string[] {
-    const champions = ['Tom Aspinall, Illia Topuria, JDM, Alex Pereira'];
-    return champions;
+    // const streamArray = StreamArray.withParser();
+    const reader = fs.createReadStream('./src/db/fighters.json', { encoding: 'utf-8' });
+    let fighters = reader.read().toString();
+    fighters = JSON.parse(fighters);
+    return fighters;
   }
 
   addFighter(createFighterDto: CreateFighterDto): CreateFighterDto {
-    const fighters = ['New Fighter 1, New Fighter 2'];
-    const fighter = { ...createFighterDto };
+    // console.log('Adding fighter:', JSON.stringify(createFighterDto, null, 2));
+    const writer = fs.createWriteStream('./src/db/fighters.json', { flags: 'a'});
+    console.log('Writer Stream:', writer);
+    // writer.write(JSON.stringify(createFighterDto, null, 2));
+
+    // const fighter = { ...createFighterDto };
+    const fighter = new CreateFighterDto();
     return fighter;
   }
 }
