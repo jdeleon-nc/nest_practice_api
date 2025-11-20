5aa7304da9ac475647e9f8c1955676e8e0252985
Jeremy de Leon <jeremy.deleon@norimaconsulting.com>
2025-11-20 12:49:28 -0600

    fix mma module format + add fighter entity reference

--- src/mma/mma.module.ts
+++ src/mma/mma.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { FighterService } from 'src/services/fighter/fighter.service';
 import { MmaController } from './mma.controller';
 import { CreateFighterDto } from './dtos/create-fighter.dto';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Fighter } from 'src/db/entities/fighter.entity';
 
-@Module({})
-export class MmaModule {
-  classes: [MmaController];
-  providers: [FighterService];
-}
+@Module({
+  imports: [TypeOrmModule.forFeature([Fighter])],
+  controllers: [MmaController],
+  providers: [FighterService],
+})
+export class MmaModule {}
