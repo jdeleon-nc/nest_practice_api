c16449ba79188ec84aa696f78fee52c15968e465
Jeremy de Leon <jeremy.deleon@norimaconsulting.com>
2025-11-20 14:27:15 -0600

    change id to lowercase

--- src/mma/db/entities/fighter.entity.ts
+++ src/mma/db/entities/fighter.entity.ts
@@ -3,7 +3,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 @Entity()
 export class Fighter {
   @PrimaryGeneratedColumn()
-  Id: number;
+  id: number;
 
   @Column()
   firstName: string;
